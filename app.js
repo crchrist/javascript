1575ffc291c6e7dd7895e33101c8b1947a4b1fc0
crchrist <codychrist@hotmail.com>
2021-04-15 08:57:16 -0700

    arrays: create 3 for loops

---
+++ app.js
@@ -0,0 +1,19 @@
+'usestrict';
+
+console.log(5+5);
+
+console.log("I don't like anchovies")
+
+console.log(parseInt('33'));
+console.log(parseFloat('3.1'));
+
+// Assingment Operators
+let name = "Cody"
+let age = 32
+
+console.log(name.replace('y', 'e'))
+
+console.log (5==5);
+
+let doesEqual = 5==5;
+console.log(doesEqual);
---
+++ arrays.js
@@ -0,0 +1,88 @@
+const favoriteAritst = ["The Beatles, AJR, Macklemore, G-Eazy"];
+const randomEmojis = ["🐶", "🚙", "🏈", "👩‍🍳"];
+const eliteCoders = ["Sebastian", "Tyler", "Danny", "Ariel", "Janeth", "Dave", "Jesse", "Cody"];
+console.log(eliteCoders)
+const theDevLeague = ["Joseph", "Shawn", "Chris", "Jason", "Avery", "Tibor", "Charlene", "Stephen"];
+const mechaPodzilla = ["Brandon", "Aaron", "Thomas", "Brianna", "Paul", "Maurice", "Evan", "Amanda"];
+const reallyCoolPodName = ["Jordan", "Anthony", "Vinny", "Edison", "Edward", "Jeremy", "Omar"];
+const my2DArray = [[1,2,3], [4,5,6], [7,8,9]];
+
+const coders = []
+for(let i=0; i < eliteCoders.length; i++) {
+    let coders = eliteCoders[i];
+}
+console.log(coders)
+
+// Methods
+// let animalLength = animal.length;
+// console.log(animalLength);
+// // 5
+// const carsAndProfessionals = cars.concat(professionals);
+// console.log(carsAndProfessionals);
+// // ["🚗", "🚙", "🏎", "🚕", "🚓", "👨🏽‍🏫", "👩‍🍳", "👨🏽‍🌾", "👨🏼‍⚖️"]
+// console.log(theDevLeague.indexOf('Brianna Giorgi'))
+// // 6
+// console.log(theDevLeague);
+// // ["Paula Bauerman", "Brandon Hall", "Thomas Duong", "Paul Knick", "Evan Woodworth", "Aaron Prince", "Brianna Giorgi", "Maurice Chevez", "Amanda Posey"]0: "Paula Bauerman"1: "Brandon Hall"2: "Thomas Duong"3: "Paul Knick"4: "Evan Woodworth"5: "Aaron Prince"6: "Brianna Giorgi"7: "Maurice Chevez"8: "Amanda Posey"length: 9__proto__: Array(0)
+// console.log(theDevLeague[6]);
+// // "Brianna Giorgi"
+// console.log(theDevLeague.indexOf('brianna giorgi'));
+// // -1
+// console.log(professionals.pop());
+// // "👨🏼‍⚖️"
+// console.log(professionals);
+// //  ["👨🏽‍🏫", "👩‍🍳", "👨🏽‍🌾"]
+// console.log(professionals.push('👨🏼‍⚖️'));
+// // 4
+// console.log(professionals);
+// // ["👨🏽‍🏫", "👩‍🍳", "👨🏽‍🌾", "👨🏼‍⚖️"]
+// console.log(cars.includes('Rome'));
+// // false
+// console.log(cars.includes('🚓'));
+// // true
+// console.log(numbers);
+// //  [1, 66, 48, 9, 23, 19, 20, 66, 30]
+// console.log(numbers.toString());
+// // "1,66,48,9,23,19,20,66,30"
+// let stringOfNumbers = numbers.toString();
+// console.log(stringOfNumbers);
+// "1,66,48,9,23,19,20,66,30"
+// console.log(stringOfNumbers.split(','));
+// // ["1", "66", "48", "9", "23", "19", "20", "66", "30"]
+// let moreStrings = "1-66-48-9-23-19-20-66-30";
+// // console.log(moreStrings.split('-'));
+// ["1", "66", "48", "9", "23", "19", "20", "66", "30"]
+
+// concat: ƒ concat()
+// constructor: ƒ Array()
+// copyWithin: ƒ copyWithin()
+// entries: ƒ entries()
+// every: ƒ every()
+// fill: ƒ fill()
+// filter: ƒ filter()
+// find: ƒ find()
+// findIndex: ƒ findIndex()
+// flat: ƒ flat()
+// flatMap: ƒ flatMap()
+// forEach: ƒ forEach()
+// includes: ƒ includes()
+// indexOf: ƒ indexOf()
+// join: ƒ join()
+// keys: ƒ keys()
+// lastIndexOf: ƒ lastIndexOf()
+// length: 0
+// map: ƒ map()
+// pop: ƒ pop()
+// push: ƒ push()
+// reduce: ƒ reduce()
+// reduceRight: ƒ reduceRight()
+// reverse: ƒ reverse()
+// shift: ƒ shift()
+// slice: ƒ slice()
+// some: ƒ some()
+// sort: ƒ sort()
+// splice: ƒ splice()
+// toLocaleString: ƒ toLocaleString()
+// toString: ƒ toString()
+// unshift: ƒ unshift()
+// values: ƒ values()
